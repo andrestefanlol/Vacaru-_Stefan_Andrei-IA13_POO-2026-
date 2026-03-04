0837e54a34056fc9d03e8cf5a24df4c81d617722
Andrei Vacaru <andreivacaru53@gmail.com>
2026-03-04 17:58:00 +0200

    extra

--- Lab 3/extra/canvas.cpp
+++ Lab 3/extra/canvas.cpp
@@ -1,1 +1,61 @@
 #include "canvas.h"
+#include <iostream>
+#include <stdarg.h>
+void Canvas::clear()
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			mat[i][j] = ' ';
+		}
+	}
+}
+Canvas::Canvas(int lini, int coloane)
+{
+	Canvas::width = coloane;
+	Canvas::height = lini;
+	mat = new char* [height];
+	for (int i = 0; i < height; i++)
+	{
+		mat[i] = new char[width];
+	}
+	Canvas::clear();
+}
+Canvas::~Canvas()
+{
+	for (int i = 0; i < height; i++)
+	{
+		delete[] mat[i];
+	}
+	delete[] mat;
+}
+void Canvas::set_pixel(int x, int y, char value)
+{
+	if (x >= 0 && x < width && y >= 0 && y < height)
+		mat[y][x] = value;
+}
+void Canvas::set_pixels(int count, ...)
+{
+	va_list args;
+	va_start(args, count);
+	for (int i = 0; i < count; i++)
+	{
+		int x = va_arg(args, int);
+		int y = va_arg(args, int);
+		char value = (char)va_arg(args, int);
+		set_pixel(x, y, value);
+	}
+	va_end(args);
+}
+void Canvas::print() const
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			std::cout << mat[i][j];
+		}
+		std::cout << std::endl;
+	}
+}
--- Lab 3/extra/canvas.h
+++ Lab 3/extra/canvas.h
@@ -1,8 +1,16 @@
 #pragma once
 class Canvas {
+private:
+    int width;
+    int height;
+    char ** mat;
 public:
     Canvas(int lines, int columns);
+	~Canvas();
+    // contructor will call the clear method
+    // set the character at the position x and y to value
     void set_pixel(int x, int y, char value);
+    // tuples of x, y, value
     void set_pixels(int count, ...);
     void clear();
     void print() const;
